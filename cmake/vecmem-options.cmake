dbe16a5c57e29cc400c0d193844e1e7aa0e6461e
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-02-10 16:12:55 +0100

    Copied the code for using HIP files from atlas/atlasexternals.
    Just minimally adjusting them for the VecMem project.

--- cmake/vecmem-options.cmake
+++ cmake/vecmem-options.cmake
@@ -4,15 +4,23 @@
 #
 # Mozilla Public License Version 2.0
 
-# Look for CUDA.
-include( CheckLanguage )
-check_language( CUDA )
+# Look for the supported GPU languages.
+include( "${CMAKE_CURRENT_LIST_DIR}/vecmem-check-language.cmake" )
+vecmem_check_language( CUDA )
+vecmem_check_language( HIP )
 
 # Set up the project's options.
 include( CMakeDependentOption )
 
+# Flag specifying whether CUDA support should be built.
 cmake_dependent_option( VECMEM_BUILD_CUDA_LIBRARY
    "Build the vecmem::cuda library" ON
    "CMAKE_CUDA_COMPILER" OFF )
 
+# Flag specifying whether HIP support should be built.
+cmake_dependent_option( VECMEM_BUILD_HIP_LIBRARY
+   "Build the vecmem::hip library" ON
+   "CMAKE_HIP_COMPILER" OFF )
+
+# Flag specifying whether SYCL support should be built.
 option( VECMEM_BUILD_SYCL_LIBRARY "Build the vecmem::sycl library" OFF )
