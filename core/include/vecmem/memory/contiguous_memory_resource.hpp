b5d3885c58fe114a4cf9fd31f98034474a3482ff
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2021-03-03 14:27:48 +0100

    Implement a contiguous downstream memory resource
    There are a lot of use cases in which you might want to allocate
    contiguous blocks of memory, but this is not supported by default by
    many allocators. This new downstream memory resource guarantees that
    consecutive allocations will be exactly contiguous in memory, which
    makes it easier to allocate memory with efficient data copies and
    locality in mind.

---
+++ core/include/vecmem/memory/contiguous_memory_resource.hpp
@@ -0,0 +1,72 @@
+/**
+ * VecMem project, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+#include "vecmem/memory/resources/memory_resource.hpp"
+
+#include <cstddef>
+
+namespace vecmem {
+    /**
+     * @brief Downstream allocator that ensures that allocations are contiguous.
+     *
+     * When programming for co-processors, it is often desriable to keep
+     * allocations contiguous. This downstream allocator fills that need. When
+     * configured with an upstream memory resource, it will start out by
+     * allocating a single, large, chunk of memory from the upstream. Then, it
+     * will hand out pointers along that memory in a contiguous fashion. This
+     * allocator guarantees that each consecutive allocation will start right at
+     * the end of the previous.
+     *
+     * @note The allocation size on the upstream allocator is also the maximum
+     * amount of memory that can be allocated from the contiguous memory
+     * resource.
+     */
+    class contiguous_memory_resource : public memory_resource {
+    public:
+        /**
+         * @brief Constructs the contiguous memory resource.
+         *
+         * @param[in] upstream The upstream memory resource to use.
+         * @param[in] size The size of memory to allocate upstream.
+         */
+        contiguous_memory_resource(
+            memory_resource & upstream,
+            std::size_t size
+        );
+
+        /**
+         * @brief Deconstruct the contiguous memory resource.
+         *
+         * This method deallocates the arena memory on the upstream allocator.
+         */
+        ~contiguous_memory_resource();
+    private:
+        virtual void * do_allocate(
+            std::size_t,
+            std::size_t
+        ) override;
+
+        virtual void do_deallocate(
+            void * p,
+            std::size_t,
+            std::size_t
+        ) override;
+
+        virtual bool do_is_equal(
+            const memory_resource &
+        ) const noexcept override;
+
+        memory_resource & m_upstream;
+        const std::size_t m_size;
+        void * const m_begin;
+        void * const m_end;
+        void * m_next;
+    };
+}
---
+++ core/src/memory/contiguous_memory_resource.cpp
@@ -0,0 +1,88 @@
+/**
+ * VecMem project, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#include "vecmem/memory/resources/memory_resource.hpp"
+#include "vecmem/memory/contiguous_memory_resource.hpp"
+
+#include <cstddef>
+#include <memory>
+#include <stdexcept>
+
+namespace vecmem {
+    contiguous_memory_resource::contiguous_memory_resource(
+        memory_resource & upstream,
+        std::size_t size
+    ) :
+        m_upstream(upstream),
+        m_size(size),
+        m_begin(m_upstream.allocate(m_size)),
+        m_end(static_cast<void *>(static_cast<char *>(m_begin) + size)),
+        m_next(m_begin)
+    {
+    }
+
+    contiguous_memory_resource::~contiguous_memory_resource() {
+        /*
+         * Deallocate our memory arena upstream.
+         */
+        m_upstream.deallocate(m_begin, m_size);
+    }
+
+    void * contiguous_memory_resource::do_allocate(
+        std::size_t size,
+        std::size_t
+    ) {
+        /*
+         * Calculate where the end of this current allocation would be.
+         */
+        void * next = static_cast<void *>(static_cast<char *>(m_next) + size);
+
+        /*
+         * The start of this allocation, save it in a temporary variable as we
+         * will override it later before returning.
+         */
+        void * curr = m_next;
+
+        /*
+         * If the end of this allocation is past the end of our memory space,
+         * we can't allocate, and should throw an error.
+         */
+        if (next > m_end) {
+            throw std::bad_alloc();
+        }
+
+        /*
+         * Update the start of the next allocation and return.
+         */
+        m_next = next;
+
+        return curr;
+    }
+
+    void contiguous_memory_resource::do_deallocate(
+        void *,
+        std::size_t,
+        std::size_t
+    ) {
+        /*
+         * Deallocation is a no-op for this memory resource, so we do nothing.
+         */
+        return;
+    }
+
+    bool contiguous_memory_resource::do_is_equal(
+        const memory_resource & other
+    ) const noexcept {
+        /*
+         * These memory resources are equal if and only if they are the same
+         * object.
+         */
+        return this == &other;
+    }
+
+}
