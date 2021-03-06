c360b88081b0330f1811ab35a0e87ccbd5a1df1e
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-03-06 07:45:57 +0100

    Introduced vecmem::get_data(...).
    Functions with this name should be in place from now on to conveniently
    construct "simple data objects" from the vecmem containers that can be
    passed to device code.

--- core/include/vecmem/containers/device_vector_data.hpp
+++ core/include/vecmem/containers/device_vector_data.hpp
@@ -45,12 +45,37 @@ namespace vecmem {
       VECMEM_HOST
       device_vector_data( const std::vector< value_type, ALLOC >& vec );
 
+      /// Constructor from another type of @c device_vector_data object
+      ///
+      /// Only enabled if the wrapped type is different, but convertible to this
+      /// object's wrapped type. This complication is necessary to avoid
+      /// problems from SYCL. Which is very particular about having default
+      /// copy constructors for the types that it sends to kernels.
+      ///
+      template< typename OTHERTYPE,
+                std::enable_if_t< ( ! std::is_same< TYPE, OTHERTYPE >::value ) &&
+                                  std::is_convertible< OTHERTYPE*, TYPE* >::value,
+                                  bool > = true >
+      VECMEM_HOST_AND_DEVICE
+      device_vector_data( const device_vector_data< OTHERTYPE >& parent );
+
       /// Size of the array in memory
       std::size_t m_size;
       /// Pointer to the start of the memory block/array
       pointer m_ptr;
 
    }; // struct device_vector_data
+
+   /// Helper function creating a @c vecmem::device_vector_data object
+   template< typename TYPE, typename ALLOC >
+   VECMEM_HOST
+   device_vector_data< TYPE >
+   get_data( std::vector< TYPE, ALLOC >& vec );
+   /// Helper function creating a @c vecmem::device_vector_data object
+   template< typename TYPE, typename ALLOC >
+   VECMEM_HOST
+   device_vector_data< const TYPE >
+   get_data( const std::vector< TYPE, ALLOC >& vec );
 
 } // namespace vecmem
 
--- core/include/vecmem/containers/device_vector_data.ipp
+++ core/include/vecmem/containers/device_vector_data.ipp
@@ -34,4 +34,32 @@ namespace vecmem {
 
    }
 
+   template< typename TYPE >
+   template< typename OTHERTYPE,
+             std::enable_if_t< ( ! std::is_same< TYPE, OTHERTYPE >::value ) &&
+                               std::is_convertible< OTHERTYPE*, TYPE* >::value,
+                               bool > >
+   VECMEM_HOST_AND_DEVICE
+   device_vector_data< TYPE >::
+   device_vector_data( const device_vector_data< OTHERTYPE >& parent )
+   : m_size( parent.m_size ), m_ptr( parent.m_ptr ) {
+
+   }
+
+   template< typename TYPE, typename ALLOC >
+   VECMEM_HOST
+   device_vector_data< TYPE >
+   get_data( std::vector< TYPE, ALLOC >& vec ) {
+
+      return device_vector_data< TYPE >( vec );
+   }
+
+   template< typename TYPE, typename ALLOC >
+   VECMEM_HOST
+   device_vector_data< const TYPE >
+   get_data( const std::vector< TYPE, ALLOC >& vec ) {
+
+      return device_vector_data< const TYPE >( vec );
+   }
+
 } // namespace vecmem
