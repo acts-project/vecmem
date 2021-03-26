0c8c5da7f864cdb45e94650cd621e7595952b927
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-03-26 22:29:20 +0100

    Added some simple tests for vecmem::data::jagged_vector_buffer.
    Testing just the most trivial aspects of its behaviour, similar to
    how vecmem::data::vector_buffer is tested only in a very simple way
    as part of the "core tests".

--- tests/core/test_core_device_containers.cpp
+++ tests/core/test_core_device_containers.cpp
@@ -6,10 +6,13 @@
  */
 
 // Local include(s).
+#include "vecmem/containers/data/jagged_vector_buffer.hpp"
 #include "vecmem/containers/data/jagged_vector_view.hpp"
 #include "vecmem/containers/data/vector_buffer.hpp"
 #include "vecmem/containers/data/vector_view.hpp"
+#include "vecmem/containers/jagged_vector.hpp"
 #include "vecmem/containers/vector.hpp"
+#include "vecmem/memory/contiguous_memory_resource.hpp"
 #include "vecmem/memory/host_memory_resource.hpp"
 
 // GoogleTest include(s).
@@ -21,6 +24,10 @@
 
 /// Test case for the custom device container types
 class core_device_container_test : public testing::Test {
+
+protected:
+   /// Memory resource for the test(s)
+   vecmem::host_memory_resource m_resource;
 
 }; // class core_device_container_test
 
@@ -64,9 +71,8 @@ TEST_F( core_device_container_test, vector_buffer ) {
    auto host_data = vecmem::get_data( host_vector );
 
    // Create an "owning copy" of the host vector's memory.
-   vecmem::host_memory_resource resource;
    vecmem::data::vector_buffer< int >
-      device_data( host_data.m_size, resource );
+      device_data( host_data.m_size, m_resource );
    memcpy( device_data.m_ptr, host_data.m_ptr,
            host_data.m_size * sizeof( int ) );
 
@@ -74,4 +80,43 @@ TEST_F( core_device_container_test, vector_buffer ) {
    EXPECT_EQ( device_data.m_size, host_vector.size() );
    EXPECT_EQ( memcmp( host_data.m_ptr, device_data.m_ptr,
                       host_data.m_size * sizeof( int ) ), 0 );
+}
+
+/// Test(s) for @c vecmem::data::jagged_vector_buffer
+TEST_F( core_device_container_test, jagged_vector_buffer ) {
+
+   // Create a dummy jagged vector in regular host memory.
+   std::vector< std::vector< int > > host_vector {
+      { 1, 2, 3, 4, 5 },
+      { 6, 7 },
+      { 8, 9, 10, 11 },
+      { 12, 13, 14, 15, 16, 17, 18 },
+      {},
+      { 19, 20 } };
+   auto host_data = vecmem::get_data( host_vector, &m_resource );
+
+   // Set up an "alternative" memory resource for the test.
+   vecmem::contiguous_memory_resource cresource( m_resource, 16384 );
+
+   // Create a buffer to hold the same data.
+   vecmem::data::jagged_vector_buffer< int >
+      device_data1( host_data, m_resource );
+   vecmem::data::jagged_vector_buffer< int >
+      device_data2( host_data, m_resource, &cresource );
+
+   // Test the internal state of the buffer.
+   EXPECT_EQ( device_data1.m_ptr, device_data1.host_ptr() );
+   EXPECT_EQ( device_data1.m_size, host_vector.size() );
+   EXPECT_NE( device_data2.m_ptr, device_data2.host_ptr() );
+   EXPECT_EQ( device_data2.m_size, host_vector.size() );
+   for( std::size_t i = 0; i < host_vector.size(); ++i ) {
+      EXPECT_EQ( device_data1.host_ptr()[ i ].m_size, host_vector[ i ].size() );
+      EXPECT_EQ( device_data2.host_ptr()[ i ].m_size, host_vector[ i ].size() );
+   }
+   for( std::size_t i = 0; i < host_vector.size() - 1; ++i ) {
+      EXPECT_EQ( device_data1.host_ptr()[ i ].m_ptr + host_vector[ i ].size(),
+                 device_data1.host_ptr()[ i + 1 ].m_ptr );
+      EXPECT_EQ( device_data2.host_ptr()[ i ].m_ptr + host_vector[ i ].size(),
+                 device_data2.host_ptr()[ i + 1 ].m_ptr );
+   }
 }
