b6916a5bd87a578f33745c04b5eb94d5574b8c26
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-03-26 14:50:00 +0100

    Updated the tests to the new interface of vecmem::jagged_device_vector.
    Introducing some testing for the iterator interface of the type.

--- tests/core/test_core_jagged_vector_view.cpp
+++ tests/core/test_core_jagged_vector_view.cpp
@@ -53,51 +53,76 @@ TEST_F(core_jagged_vector_view_test, row_size) {
 }
 
 TEST_F(core_jagged_vector_view_test, two_d_access) {
-    EXPECT_EQ(m_jag.at(0, 0), 1);
-    EXPECT_EQ(m_jag.at(0, 1), 2);
-    EXPECT_EQ(m_jag.at(0, 2), 3);
-    EXPECT_EQ(m_jag.at(0, 3), 4);
-    EXPECT_EQ(m_jag.at(1, 0), 5);
-    EXPECT_EQ(m_jag.at(1, 1), 6);
-    EXPECT_EQ(m_jag.at(2, 0), 7);
-    EXPECT_EQ(m_jag.at(2, 1), 8);
-    EXPECT_EQ(m_jag.at(2, 2), 9);
-    EXPECT_EQ(m_jag.at(2, 3), 10);
+    EXPECT_EQ(m_jag.at(0).at(0), 1);
+    EXPECT_EQ(m_jag.at(0).at(1), 2);
+    EXPECT_EQ(m_jag.at(0).at(2), 3);
+    EXPECT_EQ(m_jag.at(0).at(3), 4);
+    EXPECT_EQ(m_jag.at(1).at(0), 5);
+    EXPECT_EQ(m_jag.at(1).at(1), 6);
+    EXPECT_EQ(m_jag.at(2).at(0), 7);
+    EXPECT_EQ(m_jag.at(2).at(1), 8);
+    EXPECT_EQ(m_jag.at(2).at(2), 9);
+    EXPECT_EQ(m_jag.at(2).at(3), 10);
 }
 
 TEST_F(core_jagged_vector_view_test, two_d_access_const) {
-    EXPECT_EQ(m_jag.at(0, 0), 1);
-    EXPECT_EQ(m_jag.at(0, 1), 2);
-    EXPECT_EQ(m_jag.at(0, 2), 3);
-    EXPECT_EQ(m_jag.at(0, 3), 4);
-    EXPECT_EQ(m_jag.at(1, 0), 5);
-    EXPECT_EQ(m_jag.at(1, 1), 6);
-    EXPECT_EQ(m_jag.at(2, 0), 7);
-    EXPECT_EQ(m_jag.at(2, 1), 8);
-    EXPECT_EQ(m_jag.at(2, 2), 9);
-    EXPECT_EQ(m_jag.at(2, 3), 10);
+    const vecmem::jagged_device_vector<int>& jag = m_jag;
+    EXPECT_EQ(jag.at(0).at(0), 1);
+    EXPECT_EQ(jag.at(0).at(1), 2);
+    EXPECT_EQ(jag.at(0).at(2), 3);
+    EXPECT_EQ(jag.at(0).at(3), 4);
+    EXPECT_EQ(jag.at(1).at(0), 5);
+    EXPECT_EQ(jag.at(1).at(1), 6);
+    EXPECT_EQ(jag.at(2).at(0), 7);
+    EXPECT_EQ(jag.at(2).at(1), 8);
+    EXPECT_EQ(jag.at(2).at(2), 9);
+    EXPECT_EQ(jag.at(2).at(3), 10);
 }
 
 TEST_F(core_jagged_vector_view_test, mutate) {
-    m_jag.at(0, 0) *= 2;
-    m_jag.at(0, 1) *= 2;
-    m_jag.at(0, 2) *= 2;
-    m_jag.at(0, 3) *= 2;
-    m_jag.at(1, 0) *= 2;
-    m_jag.at(1, 1) *= 2;
-    m_jag.at(2, 0) *= 2;
-    m_jag.at(2, 1) *= 2;
-    m_jag.at(2, 2) *= 2;
-    m_jag.at(2, 3) *= 2;
+    m_jag.at(0).at(0) *= 2;
+    m_jag.at(0).at(1) *= 2;
+    m_jag.at(0).at(2) *= 2;
+    m_jag.at(0).at(3) *= 2;
+    m_jag.at(1).at(0) *= 2;
+    m_jag.at(1).at(1) *= 2;
+    m_jag.at(2).at(0) *= 2;
+    m_jag.at(2).at(1) *= 2;
+    m_jag.at(2).at(2) *= 2;
+    m_jag.at(2).at(3) *= 2;
 
-    EXPECT_EQ(m_jag.at(0, 0), 2 * 1);
-    EXPECT_EQ(m_jag.at(0, 1), 2 * 2);
-    EXPECT_EQ(m_jag.at(0, 2), 2 * 3);
-    EXPECT_EQ(m_jag.at(0, 3), 2 * 4);
-    EXPECT_EQ(m_jag.at(1, 0), 2 * 5);
-    EXPECT_EQ(m_jag.at(1, 1), 2 * 6);
-    EXPECT_EQ(m_jag.at(2, 0), 2 * 7);
-    EXPECT_EQ(m_jag.at(2, 1), 2 * 8);
-    EXPECT_EQ(m_jag.at(2, 2), 2 * 9);
-    EXPECT_EQ(m_jag.at(2, 3), 2 * 10);
+    EXPECT_EQ(m_jag.at(0).at(0), 2 * 1);
+    EXPECT_EQ(m_jag.at(0).at(1), 2 * 2);
+    EXPECT_EQ(m_jag.at(0).at(2), 2 * 3);
+    EXPECT_EQ(m_jag.at(0).at(3), 2 * 4);
+    EXPECT_EQ(m_jag.at(1).at(0), 2 * 5);
+    EXPECT_EQ(m_jag.at(1).at(1), 2 * 6);
+    EXPECT_EQ(m_jag.at(2).at(0), 2 * 7);
+    EXPECT_EQ(m_jag.at(2).at(1), 2 * 8);
+    EXPECT_EQ(m_jag.at(2).at(2), 2 * 9);
+    EXPECT_EQ(m_jag.at(2).at(3), 2 * 10);
+}
+
+TEST_F(core_jagged_vector_view_test, iterator) {
+    std::size_t i = 0;
+    for( auto itr = m_jag.begin(); itr != m_jag.end(); ++itr ) {
+        i += itr->size();
+    }
+    EXPECT_EQ( i, 16 );
+}
+
+TEST_F(core_jagged_vector_view_test, reverse_iterator) {
+    std::size_t i = 0;
+    for( auto itr = m_jag.rbegin(); itr != m_jag.rend(); ++itr ) {
+        i += itr->size();
+    }
+    EXPECT_EQ( i, 16 );
+}
+
+TEST_F(core_jagged_vector_view_test, value_iteration) {
+    std::size_t i = 0;
+    for( const auto& innerv : m_jag ) {
+        i += innerv.size();
+    }
+    EXPECT_EQ( i, 16 );
 }
