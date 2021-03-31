01e4eb42709e9267e346a45b1657364188574bc9
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-03-31 12:07:53 +0200

    Made the jagged vector test more similar to the 1D vector test.
    This is required to make it easier to re-use the same CUDA code
    for testing the explicit copies of jagged vectors between the host
    and the device.

--- tests/cuda/test_cuda_jagged_vector_view.cpp
+++ tests/cuda/test_cuda_jagged_vector_view.cpp
@@ -9,9 +9,10 @@
 #include "test_cuda_jagged_vector_view_kernels.cuh"
 
 #include "vecmem/memory/cuda/managed_memory_resource.hpp"
+
+#include "vecmem/containers/array.hpp"
 #include "vecmem/containers/vector.hpp"
 #include "vecmem/containers/jagged_vector.hpp"
-#include "vecmem/containers/data/jagged_vector_data.hpp"
 
 #include <gtest/gtest.h>
 
@@ -19,7 +20,7 @@ class cuda_jagged_vector_view_test : public testing::Test {
     protected:
     vecmem::cuda::managed_memory_resource m_mem;
     vecmem::jagged_vector<int> m_vec;
-    vecmem::data::jagged_vector_data<int> m_data;
+    vecmem::array< int, 2 > m_constants;
 
     cuda_jagged_vector_view_test(
         void
@@ -32,28 +33,35 @@ class cuda_jagged_vector_view_test : public testing::Test {
             vecmem::vector<int>(&m_mem),
             vecmem::vector<int>({12, 13, 14, 15, 16}, &m_mem)
         }, &m_mem),
-        m_data(vecmem::get_data(m_vec, &m_mem))
+        m_constants( m_mem )
     {
+        m_constants[ 0 ] = 2; m_constants[ 1 ] = 1;
     }
 };
 
 TEST_F(cuda_jagged_vector_view_test, mutate_in_kernel) {
-    doubleJagged(m_data);
 
-    EXPECT_EQ(m_vec.at(0).at(0), 202);
-    EXPECT_EQ(m_vec.at(0).at(1), 4);
-    EXPECT_EQ(m_vec.at(0).at(2), 6);
-    EXPECT_EQ(m_vec.at(0).at(3), 8);
-    EXPECT_EQ(m_vec.at(1).at(0), 210);
-    EXPECT_EQ(m_vec.at(1).at(1), 12);
-    EXPECT_EQ(m_vec.at(2).at(0), 214);
-    EXPECT_EQ(m_vec.at(2).at(1), 16);
-    EXPECT_EQ(m_vec.at(2).at(2), 18);
-    EXPECT_EQ(m_vec.at(2).at(3), 20);
-    EXPECT_EQ(m_vec.at(3).at(0), 222);
-    EXPECT_EQ(m_vec.at(5).at(0), 224);
-    EXPECT_EQ(m_vec.at(5).at(1), 26);
-    EXPECT_EQ(m_vec.at(5).at(2), 28);
-    EXPECT_EQ(m_vec.at(5).at(3), 30);
-    EXPECT_EQ(m_vec.at(5).at(4), 32);
+    // Create the data object describing the jagged vector.
+    auto vec_data = vecmem::get_data( m_vec );
+
+    // Run the linear transformation.
+    linearTransform( vecmem::get_data( m_constants ), vec_data, vec_data );
+
+    // Check the results.
+    EXPECT_EQ(m_vec.at(0).at(0), 214);
+    EXPECT_EQ(m_vec.at(0).at(1), 5);
+    EXPECT_EQ(m_vec.at(0).at(2), 7);
+    EXPECT_EQ(m_vec.at(0).at(3), 9);
+    EXPECT_EQ(m_vec.at(1).at(0), 222);
+    EXPECT_EQ(m_vec.at(1).at(1), 13);
+    EXPECT_EQ(m_vec.at(2).at(0), 226);
+    EXPECT_EQ(m_vec.at(2).at(1), 17);
+    EXPECT_EQ(m_vec.at(2).at(2), 19);
+    EXPECT_EQ(m_vec.at(2).at(3), 21);
+    EXPECT_EQ(m_vec.at(3).at(0), 234);
+    EXPECT_EQ(m_vec.at(5).at(0), 236);
+    EXPECT_EQ(m_vec.at(5).at(1), 27);
+    EXPECT_EQ(m_vec.at(5).at(2), 29);
+    EXPECT_EQ(m_vec.at(5).at(3), 31);
+    EXPECT_EQ(m_vec.at(5).at(4), 33);
 }
