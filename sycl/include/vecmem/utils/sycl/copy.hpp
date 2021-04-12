57b1ab7fb87f62d9fa9b7ef8ff303973e80588ca
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-04-12 15:40:40 +0200

    Introduced vecmem::sycl::copy.
    It's very similar to vecmem::cuda::copy and vecmem::hip::copy, just a
    little more complicated than those. As it needs to communicate with a
    cl::sycl::queue object to perform the copies.

---
+++ sycl/include/vecmem/utils/sycl/copy.hpp
@@ -0,0 +1,40 @@
+/*
+ * VecMem project, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+#pragma once
+
+// VecMem include(s).
+#include "vecmem/utils/copy.hpp"
+#include "vecmem/utils/sycl/queue_wrapper.hpp"
+
+namespace vecmem::sycl {
+
+   /// Specialisation of @c vecmem::copy for SYCL
+   ///
+   /// Unlike @c vecmem::cuda::copy and @c vecmem::hip::copy, this object does
+   /// have a state. As USM memory operations in SYCL happen through a
+   /// @c cl::sycl::queue object. So this object needs to point to a valid
+   /// queue object itself.
+   ///
+   class copy : public vecmem::copy {
+
+   public:
+      /// Constructor on top of a user-provided queue
+      copy( const queue_wrapper& queue = { "" } );
+
+   protected:
+      /// Perform a memory copy using CUDA
+      virtual void do_copy( std::size_t size, const void* from, void* to,
+                            type::copy_type cptype ) override;
+
+   private:
+      /// The queue that the copy operations are made with/for
+      queue_wrapper m_queue;
+
+   }; // class copy
+
+} // namespace vecmem::sycl
