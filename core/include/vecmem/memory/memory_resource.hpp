38f5ef0cc63ef123cdd3a3ea50c53a792369fd9c
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-09-06 17:43:12 +0200

    Shifted the determination of where to take the memory resource types from, to CMake.
    In this setup the build figures out already during configuration where
    the memory resource types would be picked up from. Regardless of the
    compiler being used afterwards. Since the VecMem build will link the
    selected types into its own libraries, so picking up another version
    of these types in some downstream project by mistake would lead to
    some cryptic linking errors.

--- core/include/vecmem/memory/memory_resource.hpp
+++ core/include/vecmem/memory/memory_resource.hpp
@@ -7,6 +7,15 @@
 
 #pragma once
 
+// This header can only be used in C++17 mode. So "device compilers" that
+// can't use C++17, must not see it. Which in practically all cases should
+// "just" be a question of code organisation.
+#if __cplusplus < 201700L
+#error \
+    "This header can only be used in C++17 mode. " \
+         "Ideally it should only be used by the \"host compiler\"."
+#endif  // < C++17
+
 /*
  * The purpose of this file is to provide uniform access (on a source-code
  * level) to the memory_resource type from the standard library. These are
@@ -14,18 +23,18 @@
  * depending on the GCC version used, so we try to unify them by aliassing
  * depending on the compiler feature flags.
  */
-#if __has_include(<memory_resource>)
+#if defined(VECMEM_HAVE_PMR_MEMORY_RESOURCE)
 #include <memory_resource>
 
 namespace vecmem {
 using memory_resource = std::pmr::memory_resource;
 }
-#elif __has_include(<experimental/memory_resource>)
+#elif defined(VECMEM_HAVE_EXPERIMENTAL_PMR_MEMORY_RESOURCE)
 #include <experimental/memory_resource>
 
 namespace vecmem {
 using memory_resource = std::experimental::pmr::memory_resource;
 }
 #else
-#error "vecmem requires C++17 LFTS V1 (P0220R1) component memory_resource!"
+#error "C++17 LFTS V1 (P0220R1) component memory_resource not found!?!"
 #endif
