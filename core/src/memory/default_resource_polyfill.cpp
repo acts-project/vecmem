3fb2f6147651dfaa326cd59dc94f83d7560dca4f
Paul Gessinger <paul.gessinger@cern.ch>
2021-09-23 17:23:16 +0200

    Add copyright statement to cpp file.

--- core/src/memory/default_resource_polyfill.cpp
+++ core/src/memory/default_resource_polyfill.cpp
@@ -1,3 +1,10 @@
+/** VecMem project, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
 #include <atomic>
 
 #include "vecmem/memory/memory_resource.hpp"
