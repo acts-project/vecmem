f4bf5caa27dfb72c1cf1a5d5d7c2d01de850e0d6
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2021-09-28 12:37:42 +0200

    Add a choice memory resource
    The choice memory resource allows the user to supply an implicitly
    enumerated list of upstream memory resources and a decision function
    which determines which (if any) of the upstream resources to use for
    allocation.

---
+++ core/src/memory/choice_memory_resource.cpp
@@ -0,0 +1,58 @@
+/**
+ * VecMem project, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#include "vecmem/memory/choice_memory_resource.hpp"
+
+#include <cassert>
+#include <cstddef>
+#include <vector>
+
+#include "vecmem/memory/memory_resource.hpp"
+
+namespace vecmem {
+choice_memory_resource::choice_memory_resource(
+    std::function<memory_resource &(std::size_t, std::size_t)> decision)
+    : m_decision(decision) {}
+
+void *choice_memory_resource::do_allocate(std::size_t size, std::size_t align) {
+    /*
+     * We cannot blindly allocate, because we need to keep track of which
+     * upstream allocator allocated this memory. Thus, we must also store
+     * the allocation result in a map.
+     */
+    memory_resource &res = m_decision(size, align);
+
+    void *ptr = res.allocate(size, align);
+
+    m_allocations.emplace(ptr, res);
+
+    return ptr;
+}
+
+void choice_memory_resource::do_deallocate(void *ptr, std::size_t size,
+                                           std::size_t align) {
+    /*
+     * Extract the record of which upstream resource was used to allocate the
+     * given pointer.
+     */
+    auto nh = m_allocations.extract(ptr);
+
+    /*
+     * For debug builds, throw an assertion error if we do not know this
+     * allocation.
+     */
+    assert(nh);
+
+    /*
+     * Retrieve the correct resource and deallocate the memory.
+     */
+    memory_resource &res = nh.mapped();
+
+    res.deallocate(nh.key(), size, align);
+}
+}  // namespace vecmem
