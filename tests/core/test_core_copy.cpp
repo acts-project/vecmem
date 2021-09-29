f119d401484e6ad51da36d46db47990759374a62
Attila Krasznahorkay <30694331+krasznaa@users.noreply.github.com>
2021-09-29 10:28:47 +0200

    Made it possible to copy from const vectors into non-const ones. (#110)
    In the previous version of the code one would always have to start copies
    from non-const host containers. Which was very far from ideal...
    
    Added explicit tests for making sure that these types of copies would be
    possible from now on.

--- tests/core/test_core_copy.cpp
+++ tests/core/test_core_copy.cpp
@@ -54,6 +54,32 @@ TEST_F(core_copy_test, vector) {
     }
 }
 
+/// Tests for copying 1-dimensional constant vectors
+TEST_F(core_copy_test, const_vector) {
+
+    // Create a reference vector.
+    const vecmem::vector<int> reference = {{1, 5, 6, 74, 234, 43, 22},
+                                           &m_resource};
+
+    // Create a view of its data.
+    auto reference_data = vecmem::get_data(reference);
+
+    // Make a copy of this reference.
+    auto copy_data = m_copy.to(reference_data, m_resource);
+
+    // Create device vectors over the two, and compare them.
+    vecmem::device_vector<const int> reference_vector(reference_data);
+    vecmem::device_vector<int> copy_vector(copy_data);
+    EXPECT_EQ(reference_vector.size(), copy_vector.size());
+    auto reference_itr = reference_vector.begin();
+    auto copy_itr = copy_vector.begin();
+    for (; (reference_itr != reference_vector.end()) &&
+           (copy_itr != copy_vector.end());
+         ++reference_itr, ++copy_itr) {
+        EXPECT_EQ(*reference_itr, *copy_itr);
+    }
+}
+
 /// Tests for copying jagged vectors
 TEST_F(core_copy_test, jagged_vector) {
 
@@ -75,6 +101,45 @@ TEST_F(core_copy_test, jagged_vector) {
 
     // Create device vectors over the two, and compare them.
     vecmem::jagged_device_vector<int> reference_vector(reference_data);
+    vecmem::jagged_device_vector<int> copy_vector(copy_data);
+    EXPECT_EQ(reference_vector.size(), copy_vector.size());
+    auto reference_itr = reference_vector.begin();
+    auto copy_itr = copy_vector.begin();
+    for (; (reference_itr != reference_vector.end()) &&
+           (copy_itr != copy_vector.end());
+         ++reference_itr, ++copy_itr) {
+        EXPECT_EQ(reference_itr->size(), copy_itr->size());
+        auto reference_itr2 = reference_itr->begin();
+        auto copy_itr2 = copy_itr->begin();
+        for (; (reference_itr2 != reference_itr->end()) &&
+               (copy_itr2 != copy_itr->end());
+             ++reference_itr2, ++copy_itr2) {
+            EXPECT_EQ(*reference_itr2, *copy_itr2);
+        }
+    }
+}
+
+/// Tests for copying constant jagged vectors
+TEST_F(core_copy_test, const_jagged_vector) {
+
+    // Create a reference vector.
+    const vecmem::jagged_vector<int> reference = {
+        {{{1, 2, 3, 4, 5}, &m_resource},
+         {{6, 7}, &m_resource},
+         {{8, 9, 10, 11}, &m_resource},
+         {{12, 13, 14, 15, 16, 17, 18}, &m_resource},
+         {{19}, &m_resource},
+         {{20}, &m_resource}},
+        &m_resource};
+
+    // Create a view of its data.
+    auto reference_data = vecmem::get_data(reference);
+
+    // Make a copy of this reference.
+    auto copy_data = m_copy.to(reference_data, m_resource);
+
+    // Create device vectors over the two, and compare them.
+    vecmem::jagged_device_vector<const int> reference_vector(reference_data);
     vecmem::jagged_device_vector<int> copy_vector(copy_data);
     EXPECT_EQ(reference_vector.size(), copy_vector.size());
     auto reference_itr = reference_vector.begin();
