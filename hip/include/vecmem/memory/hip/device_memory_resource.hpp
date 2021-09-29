440fa3b975c025627b047e773868447ce46f5485
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-09-29 11:21:12 +0200

    Made vecmem_hip work with symbol hiding turned on.
    This necessitated building hip_error_handling.cpp as part of the
    test executable (as well), since I really didn't want to make that
    code part of the public interface of the vecmem_hip library.

--- hip/include/vecmem/memory/hip/device_memory_resource.hpp
+++ hip/include/vecmem/memory/hip/device_memory_resource.hpp
@@ -8,11 +8,12 @@
 
 // Local include(s).
 #include "vecmem/memory/memory_resource.hpp"
+#include "vecmem/vecmem_hip_export.hpp"
 
 namespace vecmem::hip {
 
 /// Memory resource for a specific HIP device
-class device_memory_resource final : public memory_resource {
+class VECMEM_HIP_EXPORT device_memory_resource final : public memory_resource {
 
 public:
     /// Invalid/default device identifier
