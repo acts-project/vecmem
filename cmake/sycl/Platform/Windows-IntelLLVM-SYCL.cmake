fe8469f77a74ea71c5fcbe1b1e48bd558170ccc0
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-10-01 09:32:12 +0200

    Made the new SYCL language configuration work on Windows.
    With CMake's own modules in use for setting up the properties of
    DPC++ on Windows, the code had to be tweaked a bit. Including a
    change in debug.hpp, as the SYCL build now gained the MSVC specific
    definitions as a side-effect.

--- cmake/sycl/Platform/Windows-IntelLLVM-SYCL.cmake
+++ cmake/sycl/Platform/Windows-IntelLLVM-SYCL.cmake
@@ -9,3 +9,16 @@ include( Platform/Windows-IntelLLVM )
 
 # Set up the variables specifying the command line arguments of the compiler.
 __windows_compiler_intel( SYCL )
+
+# Tweak the MSVC linker flags, to make them compatible with DPC++.
+if( ${CMAKE_VERSION} VERSION_LESS 3.21 )
+   set( CMAKE_CREATE_WIN32_EXE "-Xlinker /subsystem:windows" )
+   set( CMAKE_CREATE_CONSOLE_EXE "-Xlinker /subsystem:console" )
+else()
+   set( CMAKE_SYCL_CREATE_WIN32_EXE "-Xlinker /subsystem:windows" )
+   set( CMAKE_SYCL_CREATE_CONSOLE_EXE "-Xlinker /subsystem:console" )
+endif()
+foreach( _type EXE SHARED MODULE )
+   string( REGEX REPLACE "(/machine:[a-zA-Z0-9]+)" "-Xlinker \\1"
+      CMAKE_${_type}_LINKER_FLAGS "${CMAKE_${_type}_LINKER_FLAGS}" )
+endforeach()
