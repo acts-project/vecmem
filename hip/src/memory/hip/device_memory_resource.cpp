0313e31a1a5d7ae798180d5b1caeffbaf3e5cd3b
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-10-04 18:09:19 +0200

    Updated the HIP memory resources to use vecmem::details::memory_resource_base.
    At the same time added some debug messages to them, and tried to harmonise them
    with the implementation of the other memory resources.

--- hip/src/memory/hip/device_memory_resource.cpp
+++ hip/src/memory/hip/device_memory_resource.cpp
@@ -11,31 +11,34 @@
 #include "../../utils/get_device.hpp"
 #include "../../utils/hip_error_handling.hpp"
 #include "../../utils/run_on_device.hpp"
+#include "vecmem/utils/debug.hpp"
 
 // HIP include(s).
 #include <hip/hip_runtime_api.h>
 
 namespace vecmem::hip {
 
-device_memory_resource::device_memory_resource(int device) : m_device(device) {
-
-    if (m_device == INVALID_DEVICE) {
-        m_device = details::get_device();
-    }
-}
+device_memory_resource::device_memory_resource(int device)
+    : m_device(device == INVALID_DEVICE ? details::get_device() : device) {}
 
 void* device_memory_resource::do_allocate(std::size_t nbytes, std::size_t) {
 
+    // Allocate the memory.
     void* result = nullptr;
     (details::run_on_device(m_device))([&result, nbytes]() {
         VECMEM_HIP_ERROR_CHECK(hipMalloc(&result, nbytes));
     });
+    VECMEM_DEBUG_MSG(4, "Allocated %ld bytes at %p on device %i", nbytes,
+                     result, m_device);
     return result;
 }
 
 void device_memory_resource::do_deallocate(void* ptr, std::size_t,
                                            std::size_t) {
 
+    // Free the memory.
+    VECMEM_DEBUG_MSG(4, "De-allocating memory at %p on device %i", ptr,
+                     m_device);
     (details::run_on_device(m_device))(
         [ptr]() { VECMEM_HIP_ERROR_CHECK(hipFree(ptr)); });
 }
