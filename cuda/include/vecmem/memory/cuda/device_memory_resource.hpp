8f2c612367268abb40fd8f1904d22632f75f97c4
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-10-04 16:42:51 +0200

    Updated the CUDA memory resources to use vecmem::details::memory_resource_base.
    At the same time added some additional documentation and debug
    messages to all of them.

--- cuda/include/vecmem/memory/cuda/device_memory_resource.hpp
+++ cuda/include/vecmem/memory/cuda/device_memory_resource.hpp
@@ -9,7 +9,7 @@
 #pragma once
 
 // Local include(s).
-#include "vecmem/memory/memory_resource.hpp"
+#include "vecmem/memory/details/memory_resource_base.hpp"
 #include "vecmem/vecmem_cuda_export.hpp"
 
 namespace vecmem::cuda {
@@ -21,7 +21,9 @@ namespace vecmem::cuda {
  * allocates, it does not try to manage memory in a smart way) that works
  * for CUDA device memory. Each instance is bound to a specific device.
  */
-class VECMEM_CUDA_EXPORT device_memory_resource : public memory_resource {
+class VECMEM_CUDA_EXPORT device_memory_resource
+    : public vecmem::details::memory_resource_base {
+
 public:
     /**
      * @brief Construct a CUDA device resource for a specific device.
@@ -33,16 +35,26 @@ public:
      *
      * @note The default device is resolved at resource construction time,
      * not at allocation time.
+     *
+     * @param[in] device CUDA identifier for the device to use
      */
     device_memory_resource(int device = -1);
 
 private:
+    /// @name Function(s) implemented from @c vecmem::memory_resource
+    /// @{
+
+    /// Allocate memory on the selected device
     virtual void* do_allocate(std::size_t, std::size_t) override;
-
+    /// De-allocate a previously allocated memory block on the selected device
     virtual void do_deallocate(void* p, std::size_t, std::size_t) override;
+    /// Compares @c *this for equality with @c other
+    virtual bool do_is_equal(
+        const memory_resource& other) const noexcept override;
 
-    virtual bool do_is_equal(const memory_resource&) const noexcept override;
+    /// @}
 
+    /// CUDA device identifier to use for the (de-)allocations
     const int m_device;
 
 };  // class device_memory_resource
