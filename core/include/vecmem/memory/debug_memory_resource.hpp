3e70a87ec48636830a7a43ab46c730c6b8f836c4
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2021-10-21 11:28:10 +0200

    Add a special memory resource for debugging
    This commit adds a new downstream memory resource which is designed to
    make it easier to debug memory errors when using memory resources. The
    debug memory resource can be wrapped around any other memory resource,
    and will report (through the use of exceptions) on three types of
    errors:
    
    1. Overlapping allocation errors: if an allocation is returned upstream
       which overlaps with an existing allocation, an error is raised.
    2. Invalid deallocation errors: if a deallocation request is issued
       which matches an outstanding allocation, but the allocation
       parameters (e.g. the size and alignment) are not the same, an error
       is raised.
    3. Double deallocation errors: if a deallocation request is issued for
       memory which is not an outstanding allocation, an error is raised.
    
    Please note that this class uses `std::logic_error`, and not
    `std::bad_alloc`. This is on purpose, in order to allow us to
    distinguish between different kinds of errors, and also to prevent the
    exception from being swallowed by, for example, a coalescing memory
    resource.

---
+++ core/include/vecmem/memory/debug_memory_resource.hpp
@@ -0,0 +1,54 @@
+/**
+ * VecMem project, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <unordered_map>
+
+#include "vecmem/memory/details/memory_resource_base.hpp"
+#include "vecmem/memory/memory_resource.hpp"
+
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable : 4251)
+#endif
+
+namespace vecmem {
+/**
+ * @brief This memory resource forwards allocation and deallocation requests to
+ * the upstream resource, but alerts the user of potential problems.
+ *
+ * For example, this memory resource can be used to catch overlapping
+ * allocations, double frees, invalid frees, and other memory integrity issues.
+ */
+class VECMEM_CORE_EXPORT debug_memory_resource final
+    : public details::memory_resource_base {
+public:
+    /**
+     * @brief Constructs the debug memory resource.
+     *
+     * @param[in] upstream The upstream memory resource to use.
+     */
+    debug_memory_resource(memory_resource& upstream);
+
+private:
+    virtual void* do_allocate(std::size_t, std::size_t) override;
+
+    virtual void do_deallocate(void* p, std::size_t, std::size_t) override;
+
+    memory_resource& m_upstream;
+
+    std::unordered_map<void*, std::pair<std::size_t, std::size_t>>
+        m_allocations;
+};
+}  // namespace vecmem
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
