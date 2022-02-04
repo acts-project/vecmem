82b28632bb44f79628b80e673b19a7807ae90932
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2022-02-04 14:57:23 +0100

    Only setting the nvcc specific flags if nvcc is being used.
    And setting an LLVM specific flag for -Werror in Debug mode.

--- cmake/vecmem-compiler-options-cuda.cmake
+++ cmake/vecmem-compiler-options-cuda.cmake
@@ -1,6 +1,6 @@
 # VecMem project, part of the ACTS project (R&D line)
 #
-# (c) 2021 CERN for the benefit of the ACTS project
+# (c) 2021-2022 CERN for the benefit of the ACTS project
 #
 # Mozilla Public License Version 2.0
 
@@ -19,9 +19,14 @@ set( CMAKE_CUDA_ARCHITECTURES "52" CACHE STRING
 
 # Make CUDA generate debug symbols for the device code as well in a debug
 # build.
-vecmem_add_flag( CMAKE_CUDA_FLAGS_DEBUG "-G" )
+if( "${CMAKE_CUDA_COMPILER_ID}" MATCHES "NVIDIA" )
+   vecmem_add_flag( CMAKE_CUDA_FLAGS_DEBUG "-G" )
+endif()
 
 # More rigorous tests for the Debug builds.
-if( "${CUDAToolkit_VERSION}" VERSION_GREATER_EQUAL "10.2" )
+if( ( "${CUDAToolkit_VERSION}" VERSION_GREATER_EQUAL "10.2" ) AND
+    ( "${CMAKE_CUDA_COMPILER_ID}" MATCHES "NVIDIA" ) )
    vecmem_add_flag( CMAKE_CUDA_FLAGS_DEBUG "-Werror all-warnings" )
+elseif( "${CMAKE_CUDA_COMPILER_ID}" MATCHES "Clang" )
+   vecmem_add_flag( CMAKE_CUDA_FLAGS_DEBUG "-Werror" )
 endif()
