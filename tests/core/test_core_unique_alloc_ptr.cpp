4677c0b5513b09524cbd3793af225bf14aeb30d4
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-03-31 14:15:11 +0200

    Add tests for new unique pointer copy semantics
    We have new semantics for unique pointers which allow them to be used as
    vehicles to copy memory from host to device. This commit adds a few new
    tests to our test suite which ensures that this functionality works
    correctly. It also tests these functions with non-trivially
    constructible types, which we previously were unable to handle.

--- tests/core/test_core_unique_alloc_ptr.cpp
+++ tests/core/test_core_unique_alloc_ptr.cpp
@@ -8,8 +8,17 @@
 
 #include <gtest/gtest.h>
 
+#include <cstring>
+#include <type_traits>
+
 #include "vecmem/memory/host_memory_resource.hpp"
 #include "vecmem/memory/unique_ptr.hpp"
+
+namespace {
+struct NonTriviallyConstructibleType {
+    int i = 0;
+};
+}  // namespace
 
 class CoreUniqueAllocPtrTest : public testing::Test {
 protected:
@@ -121,4 +130,68 @@ TEST_F(CoreUniqueAllocPtrTest, DeallocateSingle) {
     ptr1 = nullptr;
 
     ASSERT_EQ(ptr1, nullptr);
+}
+
+TEST_F(CoreUniqueAllocPtrTest, CheckTypeTraits) {
+    EXPECT_TRUE(std::is_trivially_copyable_v<NonTriviallyConstructibleType>);
+    EXPECT_FALSE(std::is_trivially_default_constructible_v<
+                 NonTriviallyConstructibleType>);
+}
+
+TEST_F(CoreUniqueAllocPtrTest, AllocateSingleByCopy) {
+    int src = 18;
+
+    vecmem::unique_alloc_ptr<int> ptr =
+        vecmem::make_unique_alloc<int>(mr, &src, std::memcpy);
+
+    ASSERT_NE(ptr, nullptr);
+    EXPECT_EQ(*ptr, 18);
+}
+
+TEST_F(CoreUniqueAllocPtrTest, AllocateArrayByCopy) {
+    int src[5] = {13, 61, 59, 42, 74};
+
+    vecmem::unique_alloc_ptr<int[]> ptr =
+        vecmem::make_unique_alloc<int[]>(mr, 5, src, std::memcpy);
+
+    ASSERT_NE(ptr, nullptr);
+    EXPECT_EQ(ptr[0], 13);
+    EXPECT_EQ(ptr[1], 61);
+    EXPECT_EQ(ptr[2], 59);
+    EXPECT_EQ(ptr[3], 42);
+    EXPECT_EQ(ptr[4], 74);
+}
+
+TEST_F(CoreUniqueAllocPtrTest, AllocateSingleNonConstructibleByCopy) {
+    NonTriviallyConstructibleType src;
+
+    src.i = 51;
+
+    vecmem::unique_alloc_ptr<NonTriviallyConstructibleType> ptr =
+        vecmem::make_unique_alloc<NonTriviallyConstructibleType>(mr, &src,
+                                                                 std::memcpy);
+
+    ASSERT_NE(ptr, nullptr);
+    EXPECT_EQ(ptr->i, 51);
+}
+
+TEST_F(CoreUniqueAllocPtrTest, AllocateArrayNonConstructibleByCopy) {
+    NonTriviallyConstructibleType src[5];
+
+    src[0].i = 13;
+    src[1].i = 61;
+    src[2].i = 59;
+    src[3].i = 42;
+    src[4].i = 74;
+
+    vecmem::unique_alloc_ptr<NonTriviallyConstructibleType[]> ptr =
+        vecmem::make_unique_alloc<NonTriviallyConstructibleType[]>(mr, 5, src,
+                                                                   std::memcpy);
+
+    ASSERT_NE(ptr, nullptr);
+    EXPECT_EQ(ptr[0].i, 13);
+    EXPECT_EQ(ptr[1].i, 61);
+    EXPECT_EQ(ptr[2].i, 59);
+    EXPECT_EQ(ptr[3].i, 42);
+    EXPECT_EQ(ptr[4].i, 74);
 }
