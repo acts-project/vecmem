a79c843e4793c0d94f68b760b05fad8d6dc677bf
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2022-07-14 13:51:49 +0200

    Switched to using std::aligned_alloc when it's available.
    At the same time updated the documentation and debug messages
    of the code a little.
    
    Unfortunately std::aligned_alloc is not available with MSVC
    (and on Windows in general from what I understand). So for that
    case the code implements its own aligned allocation scheme,
    which involves a bit of over-allocation.

--- core/include/vecmem/memory/host_memory_resource.hpp
+++ core/include/vecmem/memory/host_memory_resource.hpp
@@ -1,7 +1,7 @@
 /**
  * VecMem project, part of the ACTS project (R&D line)
  *
- * (c) 2021 CERN for the benefit of the ACTS project
+ * (c) 2021-2022 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -15,11 +15,11 @@
 namespace vecmem {
 
 /**
- * @brief Memory resource which wraps the malloc standard library call.
+ * @brief Memory resource which wraps standard library memory allocation calls.
  *
  * This is probably the simplest memory resource you can possibly write. It
- * is a terminal resource which does nothing but wrap malloc and free. It
- * is state-free (on the relevant levels of abstraction).
+ * is a terminal resource which does nothing but wrap @c std::aligned_alloc and
+ * @c std::free. It is state-free (on the relevant levels of abstraction).
  */
 class VECMEM_CORE_EXPORT host_memory_resource final
     : public details::memory_resource_base {
@@ -29,9 +29,10 @@ private:
     /// @{
 
     /// Allocate standard host memory
-    virtual void* do_allocate(std::size_t, std::size_t) override;
+    virtual void* do_allocate(std::size_t size, std::size_t alignment) override;
     /// De-allocate a block of previously allocated memory
-    virtual void do_deallocate(void* p, std::size_t, std::size_t) override;
+    virtual void do_deallocate(void* p, std::size_t size,
+                               std::size_t alignment) override;
     /// Compares @c *this for equality with @c other
     virtual bool do_is_equal(
         const memory_resource& other) const noexcept override;
