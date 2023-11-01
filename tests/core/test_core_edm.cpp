8819515cc4cf58a494f091fcd70884d1556b097e
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2023-11-01 10:21:26 +0100

    Started writing a container view type.
    Made vecmem::edm::buffer inherit from it.

--- tests/core/test_core_edm.cpp
+++ tests/core/test_core_edm.cpp
@@ -7,6 +7,7 @@
 
 // Local include(s).
 #include "vecmem/edm/buffer.hpp"
+#include "vecmem/edm/view.hpp"
 #include "vecmem/memory/host_memory_resource.hpp"
 #include "vecmem/utils/copy.hpp"
 
@@ -24,6 +25,18 @@ protected:
 
 };  // class core_device_container_test
 
+TEST_F(core_edm_test, simple_view) {
+
+    // Test the creation of a simple view.
+    vecmem::edm::view<vecmem::edm::type::scalar<int>,
+                      vecmem::edm::type::vector<float> >
+        view1;
+
+    vecmem::edm::view<vecmem::edm::type::scalar<const int>,
+                      vecmem::edm::type::vector<const float> >
+        view2{view1};
+}
+
 TEST_F(core_edm_test, simple_buffer) {
 
     // Test the creation of fixed sized and resizable, at the same time "simple"
@@ -32,4 +45,12 @@ TEST_F(core_edm_test, simple_buffer) {
                         vecmem::edm::type::vector<float> >
         buffer1(10, m_resource, vecmem::data::buffer_type::fixed_size),
         buffer2(10, m_resource, vecmem::data::buffer_type::resizable);
+
+    // "Create" views off of the buffers.
+    vecmem::edm::view<vecmem::edm::type::scalar<int>,
+                      vecmem::edm::type::vector<float> >
+        view1{buffer1}, view2{buffer2};
+    vecmem::edm::view<vecmem::edm::type::scalar<const int>,
+                      vecmem::edm::type::vector<const float> >
+        view3{buffer1}, view4{buffer2};
 }
