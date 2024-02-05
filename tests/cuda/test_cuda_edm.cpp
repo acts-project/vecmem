02ad2986daf9f533083dbad261e09ffb21facf22
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-02-05 15:40:36 +0100

    Fixed "variable shadowing" warnings coming from MSVC.

--- tests/cuda/test_cuda_edm.cpp
+++ tests/cuda/test_cuda_edm.cpp
@@ -1,6 +1,6 @@
 /* VecMem project, part of the ACTS project (R&D line)
  *
- * (c) 2023 CERN for the benefit of the ACTS project
+ * (c) 2023-2024 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -19,14 +19,14 @@
 #include <gtest/gtest.h>
 
 /// Host memory resource to use in the tests.
-static vecmem::cuda::host_memory_resource host_mr;
+static vecmem::cuda::host_memory_resource cuda_host_mr;
 /// Device memory resource to use in the tests.
-static vecmem::cuda::device_memory_resource device_mr;
+static vecmem::cuda::device_memory_resource cuda_device_mr;
 /// Managed memory resource to use in the tests.
-static vecmem::cuda::managed_memory_resource managed_mr;
+static vecmem::cuda::managed_memory_resource cuda_managed_mr;
 
 /// Synchronous device copy object to use in the tests.
-static vecmem::cuda::copy copy;
+static vecmem::cuda::copy cuda_copy;
 
 /// Pointer to the function filling a simple SoA container on a CUDA device.
 static void* cudaSimpleFillPtr = reinterpret_cast<void*>(&cudaSimpleFill);
@@ -39,11 +39,13 @@ static void* cudaJaggedFillPtr = reinterpret_cast<void*>(&cudaJaggedFill);
 static void* cudaJaggedModifyPtr = reinterpret_cast<void*>(&cudaJaggedModify);
 
 // Instantiate the test suites.
-INSTANTIATE_TEST_SUITE_P(
-    cuda_soa_device_tests_simple, soa_device_tests_simple,
-    testing::Values(std::tie(host_mr, device_mr, managed_mr, copy,
-                             cudaSimpleFillPtr, cudaSimpleModifyPtr)));
-INSTANTIATE_TEST_SUITE_P(
-    cuda_soa_device_tests_jagged, soa_device_tests_jagged,
-    testing::Values(std::tie(host_mr, device_mr, managed_mr, copy,
-                             cudaJaggedFillPtr, cudaJaggedModifyPtr)));
+INSTANTIATE_TEST_SUITE_P(cuda_soa_device_tests_simple, soa_device_tests_simple,
+                         testing::Values(std::tie(cuda_host_mr, cuda_device_mr,
+                                                  cuda_managed_mr, cuda_copy,
+                                                  cudaSimpleFillPtr,
+                                                  cudaSimpleModifyPtr)));
+INSTANTIATE_TEST_SUITE_P(cuda_soa_device_tests_jagged, soa_device_tests_jagged,
+                         testing::Values(std::tie(cuda_host_mr, cuda_device_mr,
+                                                  cuda_managed_mr, cuda_copy,
+                                                  cudaJaggedFillPtr,
+                                                  cudaJaggedModifyPtr)));
--- tests/hip/test_hip_edm.cpp
+++ tests/hip/test_hip_edm.cpp
@@ -1,6 +1,6 @@
 /* VecMem project, part of the ACTS project (R&D line)
  *
- * (c) 2023 CERN for the benefit of the ACTS project
+ * (c) 2023-2024 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -18,12 +18,12 @@
 #include <gtest/gtest.h>
 
 /// Host memory resource to use in the tests.
-static vecmem::hip::host_memory_resource host_mr;
+static vecmem::hip::host_memory_resource hip_host_mr;
 /// Device memory resource to use in the tests.
-static vecmem::hip::device_memory_resource device_mr;
+static vecmem::hip::device_memory_resource hip_device_mr;
 
 /// Synchronous device copy object to use in the tests.
-static vecmem::hip::copy copy;
+static vecmem::hip::copy hip_copy;
 
 /// Pointer to the function filling a simple SoA container on a HIP device.
 static void* hipSimpleFillPtr = reinterpret_cast<void*>(&hipSimpleFill);
@@ -36,11 +36,11 @@ static void* hipJaggedFillPtr = reinterpret_cast<void*>(&hipJaggedFill);
 static void* hipJaggedModifyPtr = reinterpret_cast<void*>(&hipJaggedModify);
 
 // Instantiate the test suites.
-INSTANTIATE_TEST_SUITE_P(hip_soa_device_tests_simple, soa_device_tests_simple,
-                         testing::Values(std::tie(host_mr, device_mr, host_mr,
-                                                  copy, hipSimpleFillPtr,
-                                                  hipSimpleModifyPtr)));
-INSTANTIATE_TEST_SUITE_P(hip_soa_device_tests_jagged, soa_device_tests_jagged,
-                         testing::Values(std::tie(host_mr, device_mr, host_mr,
-                                                  copy, hipJaggedFillPtr,
-                                                  hipJaggedModifyPtr)));
+INSTANTIATE_TEST_SUITE_P(
+    hip_soa_device_tests_simple, soa_device_tests_simple,
+    testing::Values(std::tie(hip_host_mr, hip_device_mr, hip_host_mr, hip_copy,
+                             hipSimpleFillPtr, hipSimpleModifyPtr)));
+INSTANTIATE_TEST_SUITE_P(
+    hip_soa_device_tests_jagged, soa_device_tests_jagged,
+    testing::Values(std::tie(hip_host_mr, hip_device_mr, hip_host_mr, hip_copy,
+                             hipJaggedFillPtr, hipJaggedModifyPtr)));
