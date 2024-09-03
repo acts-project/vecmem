7fbfa83ab34d14348541b8dfe6c4f1440fdab3ca
Attila Krasznahorkay <30694331+krasznaa@users.noreply.github.com>
2024-09-03 14:26:29 +0200

    Atomic Reference Reorganization, main branch (2024.08.08.) (#291)
    * Split vecmem::device_atomic_ref into multiple classes.
    
    Each new class is meant to be used on just one "platform",
    and vecmem::device_atomic_ref is a typedef to the one that
    is appropriate in any given situation.
    
    The selection procedure is not correct yet, the "POSIX" version
    of the code never gets selected at the moment.
    
    * Introduced vecmem::hip::device_atomic_reference.
    
    It is the same as vecmem::cuda::device_atomic_reference, with an additional
    include needed by HIP.
    
    * Added simple test for using the posix atomic reference.
    
    Not in a way yet which would allow the re-evaluation of the
    C++ compiler in client projects.
    
    * Tweaked the flags of the atomic_ref functions.
    
    The SYCL implementation never needs __host__ or __device__.
    
    The "POSIX" implementation will only ever work in host code.
    
    The CUDA and HIP implementations are now included more generally
    using the __CUDACC__ and __HIPCC__ macros. At which point the
    vecmem::cuda::device_atomic_ref implementation had to be tweaked
    to only use __threadfence() when building device code.
    
    * Moved vecmem::device_address_space out of details/
    
    Since the main declaration of vecmem::device_atomic_ref
    needs that enumeration as well.

---
+++ core/include/vecmem/memory/details/sycl_builtin_device_atomic_ref.hpp
@@ -0,0 +1,60 @@
+/*
+ * VecMem project, part of the ACTS project (R&D line)
+ *
+ * (c) 2022-2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+#pragma once
+
+// Local include(s).
+#include "vecmem/memory/device_address_space.hpp"
+
+// SYCL include(s).
+#include <CL/sycl.hpp>
+
+namespace vecmem {
+namespace sycl {
+namespace details {
+
+/// Helper trait for setting up an atomic reference on global or local memory
+///
+/// @tparam address The address space to use
+///
+template <device_address_space address>
+struct builtin_address_space {};
+
+/// Specialization for global device memory
+template <>
+struct builtin_address_space<device_address_space::global> {
+    static constexpr cl::sycl::memory_order ord =
+        cl::sycl::memory_order::relaxed;
+    static constexpr cl::sycl::memory_scope scp =
+        cl::sycl::memory_scope::device;
+    static constexpr cl::sycl::access::address_space add =
+        cl::sycl::access::address_space::global_space;
+};
+
+/// Specialization for local device memory
+template <>
+struct builtin_address_space<device_address_space::local> {
+    static constexpr cl::sycl::memory_order ord =
+        cl::sycl::memory_order::relaxed;
+    static constexpr cl::sycl::memory_scope scp =
+        cl::sycl::memory_scope::work_group;
+    static constexpr cl::sycl::access::address_space add =
+        cl::sycl::access::address_space::local_space;
+};
+
+}  // namespace details
+
+/// Atomic reference based on @c ::sycl::atomic_ref
+template <typename T,
+          device_address_space address = device_address_space::global>
+using builtin_device_atomic_ref =
+    cl::sycl::atomic_ref<T, details::builtin_address_space<address>::ord,
+                         details::builtin_address_space<address>::scp,
+                         details::builtin_address_space<address>::add>;
+
+}  // namespace sycl
+}  // namespace vecmem
--- core/include/vecmem/memory/device_atomic_ref.hpp
+++ core/include/vecmem/memory/device_atomic_ref.hpp
@@ -1,202 +1,117 @@
 /*
  * VecMem project, part of the ACTS project (R&D line)
  *
- * (c) 2022-2023 CERN for the benefit of the ACTS project
+ * (c) 2022-2024 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
 #pragma once
 
+// Local include(s).
+#include "vecmem/memory/device_address_space.hpp"
+
 // System include(s).
 #include <atomic>
 
-// vecmem includes
-#include "vecmem/memory/memory_order.hpp"
+#if defined(CL_SYCL_LANGUAGE_VERSION) || defined(SYCL_LANGUAGE_VERSION)
+#if defined(VECMEM_HAVE_SYCL_ATOMIC_REF)
+
+// Local include(s).
+#include "vecmem/memory/details/sycl_builtin_device_atomic_ref.hpp"
 
 namespace vecmem {
 
-/// Custom definition for the adress space
-enum class device_address_space { global = 0, local = 1 };
-
-}  // namespace vecmem
-
-#if (defined(CL_SYCL_LANGUAGE_VERSION) || defined(SYCL_LANGUAGE_VERSION)) && \
-    defined(VECMEM_HAVE_SYCL_ATOMIC_REF)
-
-// SYCL include(s).
-#include <CL/sycl.hpp>
-
-namespace vecmem {
-namespace details {
-template <device_address_space address>
-struct sycl_address_space {};
-
-template <>
-struct sycl_address_space<device_address_space::global> {
-    static constexpr ::sycl::memory_order ord = ::sycl::memory_order::relaxed;
-    static constexpr ::sycl::memory_scope scp = ::sycl::memory_scope::device;
-    static constexpr ::sycl::access::address_space add =
-        ::sycl::access::address_space::global_space;
-};
-template <>
-struct sycl_address_space<device_address_space::local> {
-    static constexpr ::sycl::memory_order ord = ::sycl::memory_order::relaxed;
-    static constexpr ::sycl::memory_scope scp =
-        ::sycl::memory_scope::work_group;
-    static constexpr ::sycl::access::address_space add =
-        ::sycl::access::address_space::local_space;
-};
-}  // namespace details
-
-/// @c vecmem::atomic_ref equals @c sycl::atomic_ref with "modern SYCL"
+/// Use @c vecmem::sycl::builtin_device_atomic_ref with modern SYCL code
 template <typename T,
           device_address_space address = device_address_space::global>
-using device_atomic_ref =
-    ::sycl::atomic_ref<T, details::sycl_address_space<address>::ord,
-                       details::sycl_address_space<address>::scp,
-                       details::sycl_address_space<address>::add>;
+using device_atomic_ref = sycl::builtin_device_atomic_ref<T, address>;
 
 }  // namespace vecmem
 
-#elif ((!defined(__CUDA_ARCH__)) && (!defined(__HIP_DEVICE_COMPILE__)) && \
-       (!defined(CL_SYCL_LANGUAGE_VERSION)) &&                            \
-       (!defined(SYCL_LANGUAGE_VERSION)) && __cpp_lib_atomic_ref)
+#else  // defined(VECMEM_HAVE_SYCL_ATOMIC_REF)
+
+// Local include(s).
+#include "vecmem/memory/details/sycl_custom_device_atomic_ref.hpp"
 
 namespace vecmem {
-/// @c vecmem::atomic_ref equals @c std::atomic_ref in host code with C++20
+
+/// Use @c vecmem::sycl::custom_device_atomic_ref with older SYCL code
+template <typename T,
+          device_address_space address = device_address_space::global>
+using device_atomic_ref = sycl::custom_device_atomic_ref<T, address>;
+
+}  // namespace vecmem
+
+#endif  // defined(VECMEM_HAVE_SYCL_ATOMIC_REF)
+#elif defined(__CUDACC__)
+
+// Local include(s).
+#include "vecmem/memory/details/cuda_device_atomic_ref.hpp"
+
+namespace vecmem {
+
+/// Use @c vecmem::cuda::device_atomic_ref in CUDA device code
+template <typename T,
+          device_address_space address = device_address_space::global>
+using device_atomic_ref = cuda::device_atomic_ref<T, address>;
+
+}  // namespace vecmem
+
+#elif defined(__HIPCC__)
+
+// Local include(s).
+#include "vecmem/memory/details/hip_device_atomic_ref.hpp"
+
+namespace vecmem {
+
+/// Use @c vecmem::hip::device_atomic_ref in HIP device code
+template <typename T,
+          device_address_space address = device_address_space::global>
+using device_atomic_ref = hip::device_atomic_ref<T, address>;
+
+}  // namespace vecmem
+
+#elif defined(__cpp_lib_atomic_ref)
+
+namespace vecmem {
+
+/// Use @c std::atomic_ref in host code with C++20
 template <typename T, device_address_space = device_address_space::global>
 using device_atomic_ref = std::atomic_ref<T>;
 
 }  // namespace vecmem
 
-#else
+#elif defined(VECMEM_SUPPORT_POSIX_ATOMIC_REF)
 
-// VecMem include(s).
-#include "vecmem/utils/types.hpp"
-
-// System include(s).
-#include <type_traits>
+// Local include(s).
+#include "vecmem/memory/details/posix_device_atomic_ref.hpp"
 
 namespace vecmem {
-/// Class providing atomic operations for the VecMem code
-///
-/// It is only meant to be used with primitive types. Ones that CUDA, HIP and
-/// SYCL built-in functions exist for. So no structs, or even pointers.
-///
-/// Note that it does not perform atomic operations in host code! That is only
-/// implemented with @c std::atomic_ref in C++20. With earlier C++ standards all
-/// operations in host code are performed as "regular" operations.
-///
-template <typename T, device_address_space = device_address_space::global>
-class device_atomic_ref {
 
-public:
-    /// @name Type definitions
-    /// @{
-
-    /// Type managed by the object
-    typedef T value_type;
-    /// Difference between two objects
-    typedef value_type difference_type;
-    /// Pointer to the value in global memory
-    typedef value_type* pointer;
-    /// Reference to a value given by the user
-    typedef value_type& reference;
-
-    /// @}
-
-    /// @name Check(s) on the value type
-    /// @{
-
-    static_assert(std::is_integral<value_type>::value,
-                  "vecmem::atomic_ref only accepts built-in integral types");
-
-    /// @}
-
-    /// Constructor, with a pointer to the managed variable
-    VECMEM_HOST_AND_DEVICE
-    explicit device_atomic_ref(reference ref);
-    /// Copy constructor
-    VECMEM_HOST_AND_DEVICE
-    device_atomic_ref(const device_atomic_ref& parent);
-
-    /// Disable the assignment operator
-    device_atomic_ref& operator=(const device_atomic_ref&) = delete;
-
-    /// @name Value setter/getter functions
-    /// @{
-
-    /// Assigns a value desired to the referenced object
-    ///
-    /// @see vecmem::device_atomic_ref::store
-    ///
-    VECMEM_HOST_AND_DEVICE
-    value_type operator=(value_type data) const;
-
-    /// Set the variable to the desired value
-    VECMEM_HOST_AND_DEVICE
-    void store(value_type data,
-               memory_order order = memory_order::seq_cst) const;
-    /// Get the value of the variable
-    VECMEM_HOST_AND_DEVICE
-    value_type load(memory_order order = memory_order::seq_cst) const;
-
-    /// Exchange the current value of the variable with a different one
-    VECMEM_HOST_AND_DEVICE
-    value_type exchange(value_type data,
-                        memory_order order = memory_order::seq_cst) const;
-
-    /// Compare against the current value, and exchange only if different
-    VECMEM_HOST_AND_DEVICE
-    bool compare_exchange_strong(reference expected, value_type desired,
-                                 memory_order success,
-                                 memory_order failure) const;
-    /// Compare against the current value, and exchange only if different
-    VECMEM_HOST_AND_DEVICE
-    bool compare_exchange_strong(
-        reference expected, value_type desired,
-        memory_order order = memory_order::seq_cst) const;
-
-    /// @}
-
-    /// @name Value modifier functions
-    /// @{
-
-    /// Add a chosen value to the stored variable
-    VECMEM_HOST_AND_DEVICE
-    value_type fetch_add(value_type data,
-                         memory_order order = memory_order::seq_cst) const;
-    /// Substitute a chosen value from the stored variable
-    VECMEM_HOST_AND_DEVICE
-    value_type fetch_sub(value_type data,
-                         memory_order order = memory_order::seq_cst) const;
-
-    /// Replace the current value with the specified value AND-ed to it
-    VECMEM_HOST_AND_DEVICE
-    value_type fetch_and(value_type data,
-                         memory_order order = memory_order::seq_cst) const;
-    /// Replace the current value with the specified value OR-d to it
-    VECMEM_HOST_AND_DEVICE
-    value_type fetch_or(value_type data,
-                        memory_order order = memory_order::seq_cst) const;
-    /// Replace the current value with the specified value XOR-d to it
-    VECMEM_HOST_AND_DEVICE
-    value_type fetch_xor(value_type data,
-                         memory_order order = memory_order::seq_cst) const;
-
-    /// @}
-
-private:
-    /// Pointer to the value to perform atomic operations on
-    pointer m_ptr;
-
-};  // class device_atomic_ref
+/// Use @c vecmem::posix_device_atomic_ref with POSIX threads
+template <typename T,
+          device_address_space address = device_address_space::global>
+using device_atomic_ref = posix_device_atomic_ref<T, address>;
 
 }  // namespace vecmem
 
-// Include the implementation.
-#include "vecmem/memory/impl/device_atomic_ref.ipp"
+#else
 
+// Local include(s).
+#include "vecmem/memory/details/dummy_device_atomic_ref.hpp"
+
+namespace vecmem {
+
+/// Use @c vecmem::dummy_device_atomic_ref as a fallback
+template <typename T,
+          device_address_space address = device_address_space::global>
+using device_atomic_ref = dummy_device_atomic_ref<T, address>;
+
+}  // namespace vecmem
+
+#endif
+
+// Test that the selected class would fulfill the atomic_ref concept.
 #if __cpp_concepts >= 201907L
 #include "vecmem/concepts/atomic_ref.hpp"
 static_assert(
@@ -209,5 +124,3 @@ static_assert(
     vecmem::concepts::atomic_ref<vecmem::device_atomic_ref<std::size_t> >,
     "Atomic reference on std::size_t is incompletely defined.");
 #endif
-
-#endif  // Platform selection
