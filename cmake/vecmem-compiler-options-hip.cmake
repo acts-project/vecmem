cb74458d73c7b81c5e36069100b830b2a7c24f52
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2026-02-18 17:02:45 +0100

    Removed the custom CMake code for building HIP binaries.
    Switching to rathr building HIP code using CMake's built-in
    functionality. Which is implemented scarily similarly to how
    this project's code behaved.

--- cmake/vecmem-compiler-options-hip.cmake
+++ cmake/vecmem-compiler-options-hip.cmake
@@ -1,6 +1,6 @@
 # VecMem project, part of the ACTS project (R&D line)
 #
-# (c) 2021-2023 CERN for the benefit of the ACTS project
+# (c) 2021-2026 CERN for the benefit of the ACTS project
 #
 # Mozilla Public License Version 2.0
 
@@ -11,8 +11,7 @@ include( vecmem-functions )
 set( CMAKE_HIP_STANDARD 14 CACHE STRING "The (HIP) C++ standard to use" )
 
 # Basic flags for all build modes.
-if( ( "${CMAKE_HIP_PLATFORM}" STREQUAL "hcc" ) OR
-    ( "${CMAKE_HIP_PLATFORM}" STREQUAL "amd" ) )
+if( "${CMAKE_HIP_PLATFORM}" STREQUAL "amd" )
    vecmem_add_flag( CMAKE_HIP_FLAGS "-Wall" )
    vecmem_add_flag( CMAKE_HIP_FLAGS "-Wextra" )
    vecmem_add_flag( CMAKE_HIP_FLAGS "-Wshadow" )
@@ -21,18 +20,15 @@ if( ( "${CMAKE_HIP_PLATFORM}" STREQUAL "hcc" ) OR
 endif()
 
 # Generate debug symbols for the device code as well in a debug build.
-if( ( "${CMAKE_HIP_PLATFORM}" STREQUAL "nvcc" ) OR
-    ( "${CMAKE_HIP_PLATFORM}" STREQUAL "nvidia" ) )
+if( "${CMAKE_HIP_PLATFORM}" STREQUAL "nvidia" )
    vecmem_add_flag( CMAKE_HIP_FLAGS_DEBUG "-G" )
 endif()
 
 # Fail on warnings, if asked for that behaviour.
 if( VECMEM_FAIL_ON_WARNINGS )
-   if( ( "${CMAKE_HIP_PLATFORM}" STREQUAL "hcc" ) OR
-       ( "${CMAKE_HIP_PLATFORM}" STREQUAL "amd" ) )
+   if( "${CMAKE_HIP_PLATFORM}" STREQUAL "amd" )
       vecmem_add_flag( CMAKE_HIP_FLAGS "-Werror" )
-   elseif( ( "${CMAKE_HIP_PLATFORM}" STREQUAL "nvcc" ) OR
-           ( "${CMAKE_HIP_PLATFORM}" STREQUAL "nvidia" ) )
+   elseif( "${CMAKE_HIP_PLATFORM}" STREQUAL "nvidia" )
       vecmem_add_flag( CMAKE_HIP_FLAGS "-Werror all-warnings" )
    endif()
 endif()
